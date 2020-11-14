65adc633afa85972c2573cd8c4c596dfae3985cb
P5T7 <73223017+P5T7@users.noreply.github.com>
2020-11-14 16:21:00 +0700

    Add files via upload

---
+++ usr/banner.py
@@ -0,0 +1,22 @@
+C = "\033[94;m"
+G = "\033[92;m"
+r = "\033[0;m"
+P = "\033[93;m"
+def banner():
+    print("""
+    {0}____ ____ ______ _____
+   /   //  _//_  _ //__  /
+  /___//_  /  / /     / /
+ /_/  /___/  /_/     /_/ Arwani.
+ {1}[ {2}Simple Multi-Bruteforce{1} ]{1} 
+ {1} [ {2} by:PST7{1} ]{1}
+            """.format(C,r,G))
+def menu():
+    print(f"{C}1{r} dari daftar teman")
+    print(f"{C}2{r} dari like postigan")
+    print(f"{C}3{r} dari cari nama")
+    print(f"{C}4{r} dari grup tapi cuma 100 doang")
+    print(f"{C}5{r} dari daftar teman teman")
+    print(f"{C}6{r} dari hastang ")
+    print(f"{C}7{r} lihat hasilya tolol:v")
+
